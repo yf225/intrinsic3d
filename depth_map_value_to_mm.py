383dd7c008d4d4de515a016555de8c0cca2c32bf
Will Feng (DPER) <willfeng@fb.com>
2022-12-05 13:49:21 -0800

    update

--- depth_map_value_to_mm.py
+++ depth_map_value_to_mm.py
@@ -1,12 +1,14 @@
 import cv2
 
-iphone13pro_mm_to_negated_value_ratio = 1.8527918781725887
+
+iphone13pro_mm_to_negated_value_ratio = 1.8527918781725887  # From physical measurement
 
 # depth map value file is the original depth image file from iPhone HEIC image after heif-convert extraction
 
 def depth_map_value_to_mm(depth_map_value_file_path):
     depth_map_value_np = cv2.imread(depth_map_value_file_path)
-    cv2.imwrite(depth_map_value_file_path.replace(".depth.original.png", ".depth.png"), (256 - depth_map_value_np) * iphone13pro_mm_to_negated_value_ratio)
+    depth_map_mm_np = ((256 - depth_map_value_np) * iphone13pro_mm_to_negated_value_ratio).astype(np.float16)
+    cv2.imwrite(depth_map_value_file_path.replace(".depth.original.png", ".depth.png"), depth_map_mm_np)
 
 if __name__ == "__main__":
     import sys
