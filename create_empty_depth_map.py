55def9054793491c12d52f68ec54902469f5eb39
Will Feng (DPER) <willfeng@fb.com>
2022-12-05 14:15:38 -0800

    update

--- create_empty_depth_map.py
+++ create_empty_depth_map.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def create_empty_depth_map(color_image_file_path, example_depth_map_value_file_path):
     depth_map_value_np = cv2.imread(example_depth_map_value_file_path).fill(0)
+    print(depth_map_value_np)
     cv2.imwrite(color_image_file_path.replace(".color.png", ".depth.png"), depth_map_value_np)
 
 if __name__ == "__main__":
